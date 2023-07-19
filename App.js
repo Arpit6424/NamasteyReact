0ae07e6ca5235ae45858626f3e9fdaa5be6fd045
Arpit Rajput <arpitrajput6424@gmail.com>
2023-07-19 22:33:23 +0530

    episode-04

--- App.js
+++ App.js
@@ -1,36 +1,95 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./assets/logo.png";
 
-//Recat.createElement => object => HTMLElement(render)
-const heading = React.createElement("h1", {}, "heading");
+/**
+ * 
+ * Header
+ * -Logo
+ * -Nav Items
+ * Body
+ * -Search
+ * -RestaurantContainer
+ *  - Restaurant Card
+ *    - Img
+ *    - Name of Res, Star Rating, Cuisine ,delivery time
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ * 
 
-//JSX - is not HTML in JS
-// JSX - is HTML/XML like syntax
-//parcel transpile JSX code before it reach to JS engine
-//parcel also does not transpile but parcel gives to babel to done this job
+ */
 
-//First JSX convert to React.createElement then this React.createElement(object ) render and converted to HTMlelement
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//if there are multiple line in JSX then it is mandotary to use (). inside round bracket you have to write JSX
-const jsxHeading = <h1 className="heading">Namastey React using JSX</h1>;
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
 
-// React component
-// Class Based Component - Old
-// Functional Component - New
+const RestauaantCard = (props) => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtDJX35GB0Ku9RDq5im7pGUTxBer8NNhQ4Xw&usqp=CAU"
+        alt=""
+      />
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>{props.rating}</h4>
+      <h4>{props.time}</h4>
+    </div>
+  );
+};
 
-//A function which returns a JSX is known as componet
-// or a function which return a react element is knowm as a component
+const Body = () => {
+  return (
+    <div>
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestauaantCard
+          resName="Meghana Foods"
+          cuisine="Biryani, North Indian, Asian"
+          rating="4.5"
+          time="38 minutes"
+        />
+        <RestauaantCard
+          resName="KFC"
+          cuisine="Burger,Fast Food"
+          rating="4.3"
+          time="20 minutes"
+        />
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponet = () => (
-  <>
-    <>
-      {Title}
-      <h1>Namastey React Functional Component</h1>
-    </>
-  </>
-);
-
-const Title = <h1>Title</h1>;
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponet />);
+root.render(<AppLayout />);
